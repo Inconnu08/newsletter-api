7189f0aeb85fd7ff5282f856708b5c66cd09da0a
Taufiq Rahman <taufiqrx8@gmail.com>
2022-04-09 02:10:25 +0600

    Refactor tests with pg setup

--- src/configuration.rs
+++ src/configuration.rs
@@ -2,11 +2,11 @@
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16
-    }
+    pub application_port: u16,
+}
 
- #[derive(serde::Deserialize)]
-    pub struct DatabaseSettings {
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
     pub username: String,
     pub password: String,
     pub port: u16,
@@ -23,8 +23,15 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
 impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
-        "postgres://{}:{}@{}:{}/{}",
-        self.username, self.password, self.host, self.port, self.database_name
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
         )
     }
 }
